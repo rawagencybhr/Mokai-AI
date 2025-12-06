f404be492260b7a1eaf4865d22fa34934677b039
rawagencybhr <rawagency.bhr@gmail.com>
2025-12-07 00:54:51 +0300

    UPD

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "004d87bf2316a86c"; }
+/******/ 		__webpack_require__.h = function() { return "58cb3dd9fbd31597"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
